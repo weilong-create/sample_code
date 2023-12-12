854278b4e5f84584a55ef6c385d05053c7aab46c
Vincent.Chen <4603143264f@gmail.com>
2023-12-12 14:03:56 +0800

    dropdownoptions and button fix issue

--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
-
-  constructor() { }
+  dropDownOptions:any
+  popupData:any;
+  showEditPopUp: boolean=false;
+  test=[{id:1,name:123456},{id:2,name:123456},{id:3,name:123456},{id:4,name:123456},{id:5,name:123456},,{id:6,name:123456}]
+  constructor() {this.dropDownOptions = {
+    maxHeight: 200,
+    onShown(e: any) {
+      e.component.option({
+        container: e.element,
+        hideOnParentScroll: true,
+      });
+    },
+  }; }
 
   ngOnInit(): void {
   }
-
+  openPopup() {
+    this.showEditPopUp = true;
+  }
 }
